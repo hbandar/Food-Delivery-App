de9d9e30f44f6dc545ec187c8a60262c9bde15ae
hbandar <vardha18harsha@gmail.com>
2023-06-24 01:42:49 -0400

    setup datavalidation in express

--- gofoodapp/backend/Routes/createuser.js
+++ gofoodapp/backend/Routes/createuser.js
@@ -1,15 +1,25 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/user')
+const { body, matchedData, validationResult } = require('express-validator');
 
 router.use((req, res, next) => {
     console.log('Time: ', Date.now())
     next()
   })
 
-router.post('/createuser', (req,res)=>{
+router.post('/createuser',[
+body('email').isEmail(),
+body('name').isLength({min:6}),
+body('password').isLength({ min: 5 })]
+,async (req,res)=>{
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors:errors.array()});
+    }
+    
     try{
-            User.create({
+        await User.create({
             name:req.body.name,
             password:req.body.password,
             email: req.body.email,
--- gofoodapp/backend/index.js
+++ gofoodapp/backend/index.js
@@ -8,6 +8,7 @@ mongoDB();
 app.get('/', (req,res) =>{
     res.send("Hello");
 });
+
 app.use(express.json());
 app.use('/api', require("./Routes/createuser"));
 
