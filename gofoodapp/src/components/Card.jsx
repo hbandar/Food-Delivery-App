fe392b09cb53cb3946e8984003495370d6b4305d
hbandar <vardha18harsha@gmail.com>
2023-06-24 23:09:44 -0400

    setup Home page

--- gofoodapp/src/components/Card.jsx
+++ gofoodapp/src/components/Card.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 
-export default function Card() {
+export default function Card(props) {
+
+  let options = props.options;
+  let priceOptions = Object.keys(options)
+
   return (
     <div>
         <div className="card mt-3" style={{ width: "18rem", maxHeight: "400px" }}>
-          <img src="https://www.indianhealthyrecipes.com/wp-content/uploads/2014/11/paneer-butter-masala-recipe-2.jpg" className="card-img-top" alt="..." />
+          <img src={props.imglink}/>
           <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">
-                Some Item className </p>
+                <h5 className="card-title">{props.foodName}</h5>
               <div className="container flex w-100 p-0 d-inline">
                 <select className="h-100 bg-light mx-2 d-inline">
                   {Array.from(Array(6), (e, i) => {
@@ -20,8 +22,11 @@ export default function Card() {
                   })}
                 </select>
                 <select className="h-100 mx-2 bg-light rounded d-inline">
-                  <option value="half">Half</option>
-                  <option value="full">Full</option>
+                    {priceOptions.map((data)=>{
+                      if(data!=="_id"){
+                      return <option key={data} value={data}>{data}</option>
+                      }
+                    })}
                 </select>
                 <div className="fs-5 h-100 mx-2 d-inline">Total Price</div>
                 </div>
--- gofoodapp/src/screens/Home.js
+++ gofoodapp/src/screens/Home.js
@@ -1,25 +1,148 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Navbar from "../components/Navbar";
 import Card from "../components/Card";
 import Footer from "../components/Footer";
-import Carousal from "../components/Carousal";
 
 export default function Home() {
+  const [foodCat, setfoodCat] = useState([]);
+  const [foodItem, setfoodItem] = useState([]);
+  const [search,setSearch] = useState("");
+
+  const loadData = async () => {
+    let response = await fetch("http://localhost:4000/api/foodData", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    response = await response.json();
+    // console.log(response[0],response[1]);
+    setfoodItem(response[0]);
+    setfoodCat(response[1]);
+  };
+
+  useEffect(() => {
+    loadData();
+  }, []);
+
   return (
     <div>
       <div>
         <Navbar />
       </div>
       <div>
-        <Carousal/>
+        <div
+          id="carouselslide"
+          className="carousel slide carousal-fade"
+          data-ride="carousel"
+          style={{ objectFit: "contain !important" }}
+        >
+          <div className="carousel-inner" id="carousalimage">
+            <div className="carousel-caption" style={{ zIndex: "10" }}>
+              <div className="d-flex justify-content-center">
+                <input
+                  className="form-control me-2"
+                  type="search"
+                  placeholder="Search"
+                  aria-label="Search"
+                  value = {search}
+                  onChange={(e)=>{setSearch(e.target.value)}}
+                />
+                <button className="btn btn-success" type="submit">
+                  Search
+                </button>
+              </div>
+            </div>
+            <div className="carousel-item active">
+              <img
+                className="d-block w-100"
+                src="https://source.unsplash.com/random/600x500/?momos"
+                style={{ filter: "brightness(30%" }}
+                alt="First slide"
+              />
+            </div>
+            <div className="carousel-item">
+              <img
+                className="d-block w-100"
+                src="https://source.unsplash.com/random/900x700/?momos"
+                style={{ filter: "brightness(30%" }}
+                alt="Second slide"
+              />
+            </div>
+            <div className="carousel-item">
+              <img
+                className="d-block w-100"
+                src="https://source.unsplash.com/random/900x700/?pizza"
+                style={{ filter: "brightness(30%" }}
+                alt="Third slide"
+              />
+            </div>
+          </div>
+          <a
+            className="carousel-control-prev"
+            href="#carouselExampleControls"
+            role="button"
+            data-slide="prev"
+          >
+            <span
+              className="carousel-control-prev-icon"
+              aria-hidden="true"
+            ></span>
+            <span className="sr-only">Previous</span>
+          </a>
+          <a
+            className="carousel-control-next"
+            href="#carouselExampleControls"
+            role="button"
+            data-slide="next"
+          >
+            <span
+              className="carousel-control-next-icon"
+              aria-hidden="true"
+            ></span>
+            <span className="sr-only">Next</span>
+          </a>
+        </div>
       </div>
-      <div className="m-3">
-        <Card />
-        <Card />
-        <Card />
+      <div className="container">
+        {foodCat !== [] ? (
+          foodCat.map((data) => {
+            return (
+              <div className="row mb-3">
+                <div key={data._id} className="fs-3 m-3">
+                  {data.name}
+                </div>
+                <hr />
+                {foodItem !== [] ? (
+                  foodItem
+                    .filter((itemdata) => (itemdata.CategoryName == data.name) && (itemdata.name.toLowerCase().includes(search.toLocaleLowerCase())))
+                    .map((matchedData) => {
+                      return (
+                        <div
+                          key={matchedData._id}
+                          className="col-12 col-md-6 col-lg-3"
+                        >
+                          <Card
+                            foodName={matchedData.name}
+                            options={matchedData.options[0]}
+                            imglink={matchedData.img}
+                          ></Card>
+                        </div>
+                      );
+                    })
+                ) : (
+                  <div> No data found</div>
+                )}
+              </div>
+            );
+          })
+        ) : (
+          <div>Loaaaadingggg!!!!</div>
+        )}
       </div>
       <div>
-        <Footer/>
+        <Footer />
       </div>
     </div>
   );
